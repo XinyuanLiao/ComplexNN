6bd9584a2fbe9facb0c57e55162541c4df353916
XinyuanLiao <122154029+XinyuanLiao@users.noreply.github.com>
2023-11-02 13:06:44 +0800

    Add files via upload

---
+++ Module/complexLinear.py
@@ -0,0 +1,20 @@
+import math
+import torch
+import torch.nn as nn
+
+
+class complexLinear(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super(complexLinear, self).__init__()
+        self.weight = nn.Parameter(torch.zeros(in_features, out_features, dtype=torch.cfloat))
+        self.bias = nn.Parameter(torch.zeros(1, out_features, dtype=torch.cfloat), requires_grad=bias)
+
+        torch.nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if bias:
+            bound = 1 / math.sqrt(in_features)
+            torch.nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input):
+        if not input.dtype == torch.cfloat:
+            input = torch.complex(input, torch.zeros_like(input))
+        return torch.matmul(input, self.weight.T) + self.bias
