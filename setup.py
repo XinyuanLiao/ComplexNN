5abd68db2f91eb10fd2837e67b63f3034495c5cf
XinyuanLiao <122154029+XinyuanLiao@users.noreply.github.com>
2023-11-02 23:41:02 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
-from setuptools import setup, find_packages
-
-VERSION = '0.0.1'
-DESCRIPTION = 'A toolbox for using complex valued standard network modules in PyTorch.'
+from setuptools import find_packages, setup
 
 setup(
     name="complexNN",
-    version=VERSION,
+    version="0.0.1",
+    description="A toolbox for using complex valued standard network modules in PyTorch.",
+    long_description=open("README.md").read().strip(),
+    long_description_content_type="text/markdown",
     author="Xinyuan Liao",
     author_email="liaoxinyuan@mail.nwpu.edu.cn",
-    description=DESCRIPTION,
+    url="https://github.com/XinyuanLiao/ComplexNN",
     packages=find_packages(),
-    install_requires=[],
-    keywords=['python', 'pytorch', 'complex number', 'windows', 'mac', 'neural network'],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+    install_requires=["torch"],
+    python_requires=">=3.6",
+    license="Apache-2.0 License",
+    zip_safe=False,
+    keywords="pytorch, deep learning, complex values",
+    classifiers=[""],
 )
