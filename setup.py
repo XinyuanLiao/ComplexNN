3b417af95ae0d96749fd0aae34bd90ad3c14a692
XinyuanLiao <122154029+XinyuanLiao@users.noreply.github.com>
2023-11-05 17:12:57 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,10 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="complexNN",
-    version="0.3.1",
+    version="0.3.2",
     description="A toolbox for using complex valued standard network modules in PyTorch.",
-    long_description=open("README.md").read().strip(),
-    long_description_content_type="text/markdown",
     author="Xinyuan Liao",
     author_email="liaoxinyuan@mail.nwpu.edu.cn",
     url="https://github.com/XinyuanLiao/ComplexNN",
